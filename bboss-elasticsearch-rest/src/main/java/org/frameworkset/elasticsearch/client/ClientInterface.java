15fe6065897a32ff4a2d3216a37a3d162650f54d
yinbp <yin-bp@163.com>
2018-05-18 10:04:10 +0800

    增加parentIdKey参数api，bulk map时用来指定parentid在map中的key
    增加parentIdKey参数api，bulk map时用来指定parentid在map中的key

--- bboss-elasticsearch-rest/src/main/java/org/frameworkset/elasticsearch/client/ClientInterface.java
+++ bboss-elasticsearch-rest/src/main/java/org/frameworkset/elasticsearch/client/ClientInterface.java
@@ -352,8 +352,6 @@ public interface ClientInterface {
 	public abstract String addDocuments(String indexName, String indexType, List<?> beans,String refreshOption) throws ElasticSearchException;
 	public abstract String addDocuments(String indexName, String indexType,  List<?> beans) throws ElasticSearchException;
 
-	public abstract String addDocuments(String indexName, String indexType, List<Map> beans,String docIdKey,String refreshOption) throws ElasticSearchException;
-	public abstract String addDocumentsWithIdKey(String indexName, String indexType,  List<Map> beans,String docIdKey) throws ElasticSearchException;
 
 	/**
 	 * 创建或者更新索引文档
@@ -698,16 +696,7 @@ public interface ClientInterface {
 	 */
 	public abstract String addDateDocuments(String indexName, String indexType, List<?> beans) throws ElasticSearchException;
 
-	/**
-	 * 批量创建索引,根据时间格式建立新的索引表
-	 * @param indexName
-	 * @param indexType
-	 * @param beans
-	 * @param docIdKey map中作为文档id的Key
-	 * @return
-	 * @throws ElasticSearchException
-	 */
-	public abstract String addDateDocumentsWithIdKey(String indexName, String indexType, List<Map> beans,String docIdKey) throws ElasticSearchException;
+
 
 	/**
 	 *
@@ -740,6 +729,44 @@ public interface ClientInterface {
 	 * @throws ElasticSearchException
 	 */
 	public abstract String addDateDocuments(String indexName, String indexType, List<Map> beans,String docIdKey,String refreshOption) throws ElasticSearchException;
+	/**
+	 * 批量创建索引,根据时间格式建立新的索引表
+	 * @param indexName
+	 * @param indexType
+	 * @param beans
+	 * @param docIdKey map中作为文档id的Key
+	 * @return
+	 * @throws ElasticSearchException
+	 */
+	public abstract String addDateDocumentsWithIdKey(String indexName, String indexType, List<Map> beans,String docIdKey) throws ElasticSearchException;
+	public abstract String addDocuments(String indexName, String indexType, List<Map> beans,String docIdKey,String refreshOption) throws ElasticSearchException;
+	public abstract String addDocumentsWithIdKey(String indexName, String indexType,  List<Map> beans,String docIdKey) throws ElasticSearchException;
+
+
+	/**********************/
+	/**
+	 * 批量创建索引,根据时间格式建立新的索引表
+	 * @param indexName
+	 * @param indexType
+	 * @param beans
+	 * @param docIdKey map中作为文档id的Key
+	 * @return
+	 * @throws ElasticSearchException
+	 */
+	public abstract String addDateDocuments(String indexName, String indexType, List<Map> beans,String docIdKey,String parentIdKey,String refreshOption) throws ElasticSearchException;
+	/**
+	 * 批量创建索引,根据时间格式建立新的索引表
+	 * @param indexName
+	 * @param indexType
+	 * @param beans
+	 * @param docIdKey map中作为文档id的Key
+	 * @return
+	 * @throws ElasticSearchException
+	 */
+	public abstract String addDateDocumentsWithIdKey(String indexName, String indexType, List<Map> beans,String docIdKey,String parentIdKey) throws ElasticSearchException;
+	public abstract String addDocuments(String indexName, String indexType, List<Map> beans,String docIdKey,String parentIdKey,String refreshOption) throws ElasticSearchException;
+	public abstract String addDocumentsWithIdKey(String indexName, String indexType,  List<Map> beans,String docIdKey,String parentIdKey) throws ElasticSearchException;
+
 
 	/**************************************创建或者修改文档结束**************************************************************/
 
